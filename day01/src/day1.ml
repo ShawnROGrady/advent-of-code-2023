3c02be9da969043896f455fb441dfe9f5edd7133
ShawnROGrady <shawn.r.ogrady@gmail.com>
2023-12-03 22:00:39 +0000

    day1: part2

--- day01/src/day1.ml
+++ day01/src/day1.ml
@@ -9,16 +9,17 @@ module Input = struct
   let of_string s = s |> String.split_on_char '\n' |> List.to_seq
 end
 
-let int_of_char_opt c =
-  if c >= '0' && c <= '9' then Some (Char.code c - 48) else None
-
 let two_digit x y = (10 * x) + y
 
 module Part1 = struct
+  let int_of_char_opt c =
+    match c with '0' .. '9' -> Some (Char.code c - 48) | _ -> None
+
+  let nums_of_line line =
+    line |> String.to_seq |> Seq.filter_map int_of_char_opt
+
   let value_of_line line =
-    let nums =
-      line |> String.to_seq |> Seq.filter_map int_of_char_opt |> Array.of_seq
-    in
+    let nums = line |> nums_of_line |> Array.of_seq in
     two_digit nums.(0) nums.(Array.length nums - 1)
 
   let run : Input.t -> int =
@@ -26,5 +27,28 @@ module Part1 = struct
 end
 
 module Part2 = struct
-  let run : Input.t -> int = fun _ -> failwith "unimplemented"
+  let nums_of_line line =
+    let rec loop nums = function
+      | [] -> nums
+      | ('0' .. '9' as c) :: rest -> loop ((Char.code c - 48) :: nums) rest
+      | 'o' :: 'n' :: 'e' :: rest -> loop (1 :: nums) ('e' :: rest)
+      | 't' :: 'w' :: 'o' :: rest -> loop (2 :: nums) ('o' :: rest)
+      | 't' :: 'h' :: 'r' :: 'e' :: 'e' :: rest -> loop (3 :: nums) ('e' :: rest)
+      | 'f' :: 'o' :: 'u' :: 'r' :: rest -> loop (4 :: nums) rest
+      | 'f' :: 'i' :: 'v' :: 'e' :: rest -> loop (5 :: nums) ('e' :: rest)
+      | 's' :: 'i' :: 'x' :: rest -> loop (6 :: nums) rest
+      | 's' :: 'e' :: 'v' :: 'e' :: 'n' :: rest -> loop (7 :: nums) ('n' :: rest)
+      | 'e' :: 'i' :: 'g' :: 'h' :: 't' :: rest -> loop (8 :: nums) ('t' :: rest)
+      | 'n' :: 'i' :: 'n' :: 'e' :: rest -> loop (9 :: nums) ('e' :: rest)
+      | _ :: rest -> loop nums rest
+    in
+
+    loop [] (List.of_seq @@ String.to_seq line)
+
+  let value_of_line line =
+    let nums = line |> nums_of_line |> Array.of_list in
+    two_digit nums.(Array.length nums - 1) nums.(0)
+
+  let run : Input.t -> int =
+   fun lines -> lines |> Seq.map value_of_line |> Seq.fold_left ( + ) 0
 end
--- day01/test/test_day1.ml
+++ day01/test/test_day1.ml
@@ -1,14 +1,34 @@
-let raw_input = {|1abc2
+let test_part1 () =
+  let raw_input = {|1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
-treb7uchet|}
+treb7uchet|} in
 
-let test_part1 () =
   let expected = 142
   and actual = Day1.Part1.run (Day1.Input.of_string raw_input) in
 
   Alcotest.(check int) "sum of calibration values" expected actual
 
+let test_part2 () =
+  let raw_input =
+    {|two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen|}
+  in
+
+  let expected = 281
+  and actual = Day1.Part2.run (Day1.Input.of_string raw_input) in
+
+  Alcotest.(check int) "sum of calibration values" expected actual
+
 let () =
   let open Alcotest in
-  run "Day1" [ ("part1", [ test_case "Part1" `Quick test_part1 ]) ]
+  run "Day1"
+    [
+      ("part1", [ test_case "Part1" `Quick test_part1 ]);
+      ("part2", [ test_case "Part2" `Quick test_part2 ]);
+    ]
