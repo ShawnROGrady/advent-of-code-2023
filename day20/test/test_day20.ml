80117cfe8f428e73486e47babe268bb5ec3f73c6
ShawnROGrady <shawn.r.ogrady@gmail.com>
2024-01-17 20:55:52 -0600

    day20: test part2
    Adds some test cases for part2, which I manually worked out since none
    were provided.

--- day20/test/test_day20.ml
+++ day20/test/test_day20.ml
@@ -18,7 +18,41 @@ module TestPart1 = struct
   let run_case raw_input expected () =
     let actual = Day20.Part1.run (Day20.Input.of_string raw_input) in
 
-    Alcotest.(check z) "total pulses" expected actual
+    Alcotest.(check z) "total pulses" (Z.of_int expected) actual
+
+  let make_case (name, (raw_input, expected)) =
+    Alcotest.test_case
+      (Printf.sprintf "Part1 %s" name)
+      `Quick
+      (run_case raw_input expected)
+
+  let cases =
+    [ ("Example1", (example1, 32000000)); ("Example2", (example2, 11687500)) ]
+    |> List.map make_case
+end
+
+module TestPart2 = struct
+  let example1 =
+    {|broadcaster -> a, b, c
+%a -> b
+%b -> c
+%c -> inv
+&inv -> a, rx|}
+
+  let example2 =
+    {|broadcaster -> a
+%a -> inv, con1, con2
+&inv -> b
+%b -> con1
+&con1 -> c
+%c -> d, con2
+%d -> con2
+&con2 -> rx|}
+
+  let run_case raw_input expected () =
+    let actual = Day20.Part2.run (Day20.Input.of_string raw_input) in
+
+    Alcotest.(check z) "fewest presses" (Z.of_int expected) actual
 
   let make_case (name, (raw_input, expected)) =
     Alcotest.test_case
@@ -27,19 +61,10 @@ module TestPart1 = struct
       (run_case raw_input expected)
 
   let cases =
-    [
-      ("Example1", (example1, Z.of_int 32000000));
-      ("Example2", (example2, Z.of_int 11687500));
-    ]
+    [ ("Example1", (example1, 1)); ("Example2", (example2, 5)) ]
     |> List.map make_case
 end
 
-let test_part2 () = Alcotest.skip ()
-
 let () =
   let open Alcotest in
-  run "Day19"
-    [
-      ("part1", TestPart1.cases);
-      ("part2", [ test_case "Part2" `Quick test_part2 ]);
-    ]
+  run "Day19" [ ("part1", TestPart1.cases); ("part2", TestPart2.cases) ]
